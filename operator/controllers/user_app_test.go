34cce00c3846d14fab64040d7fbb6cd946b8d0c8
Aaron Schlesinger <aaron@ecomaz.net>
2021-02-12 12:17:22 -0800

    finishing tests for the service
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- operator/controllers/user_app_test.go
+++ operator/controllers/user_app_test.go
@@ -10,7 +10,9 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
@@ -59,6 +61,7 @@ var _ = Describe("UserApp", func() {
 			Expect(cond2.Status).To(Equal(metav1.ConditionTrue))
 			Expect(cond2.Reason).To(Equal(v1alpha1.AppServiceCreated))
 
+			// check the deployment that was created
 			deployment := &appsv1.Deployment{}
 			err = cl.Get(ctx, client.ObjectKey{
 				Name:      cfg.Name,
@@ -68,6 +71,20 @@ var _ = Describe("UserApp", func() {
 			Expect(deployment.Name).To(Equal(cfg.Name))
 			Expect(len(deployment.Spec.Template.Spec.Containers)).To(Equal(1))
 			Expect(deployment.Spec.Template.Spec.Containers[0].Image).To(Equal(cfg.Image))
+
+			// check the service that was created
+			svc := &corev1.Service{}
+			err = cl.Get(ctx, client.ObjectKey{
+				Name:      cfg.Name,
+				Namespace: cfg.Namespace,
+			}, svc)
+			Expect(err).To(BeNil())
+			Expect(svc.Name).To(Equal(cfg.Name))
+			Expect(len(svc.Spec.Ports)).To(Equal(1))
+			Expect(svc.Spec.Ports[0].Protocol).To(Equal(corev1.ProtocolTCP))
+			Expect(svc.Spec.Ports[0].TargetPort.IntVal).To(Equal(cfg.Port))
+			Expect(svc.Spec.Ports[0].TargetPort.Type).To(Equal(intstr.Int))
+			Expect(svc.Spec.Ports[0].Port).To(Equal(int32(8080)))
 		})
 	})
 })
