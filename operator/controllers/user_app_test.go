8b3e53b01bf1a2bbc4a55bc7f7b354a470faa100
Aaron Schlesinger <aaron@ecomaz.net>
2021-02-12 12:10:54 -0800

    more checks
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- operator/controllers/user_app_test.go
+++ operator/controllers/user_app_test.go
@@ -2,13 +2,16 @@ package controllers
 
 import (
 	"context"
+	"time"
 
 	"github.com/go-logr/logr"
 	"github.com/kedacore/http-add-on/operator/api/v1alpha1"
 	"github.com/kedacore/http-add-on/operator/controllers/config"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
+	appsv1 "k8s.io/api/apps/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/client/fake"
 )
 
@@ -37,6 +40,34 @@ var _ = Describe("UserApp", func() {
 			}
 			err := createUserApp(ctx, cfg, cl, logger, httpso)
 			Expect(err).To(BeNil())
+			// make sure that httpso has the right conditions on it
+			Expect(len(httpso.Status.Conditions)).To(Equal(2))
+
+			cond1 := httpso.Status.Conditions[0]
+			cond1ts, err := time.Parse(time.RFC3339, cond1.Timestamp)
+			Expect(err).To(BeNil())
+			Expect(time.Now().Sub(cond1ts) >= 0).To(BeTrue())
+			Expect(cond1.Type).To(Equal(v1alpha1.Created))
+			Expect(cond1.Status).To(Equal(metav1.ConditionTrue))
+			Expect(cond1.Reason).To(Equal(v1alpha1.AppDeploymentCreated))
+
+			cond2 := httpso.Status.Conditions[1]
+			cond2ts, err := time.Parse(time.RFC3339, cond2.Timestamp)
+			Expect(err).To(BeNil())
+			Expect(time.Now().Sub(cond2ts) >= 0).To(BeTrue())
+			Expect(cond2.Type).To(Equal(v1alpha1.Created))
+			Expect(cond2.Status).To(Equal(metav1.ConditionTrue))
+			Expect(cond2.Reason).To(Equal(v1alpha1.AppServiceCreated))
+
+			deployment := &appsv1.Deployment{}
+			err = cl.Get(ctx, client.ObjectKey{
+				Name:      cfg.Name,
+				Namespace: cfg.Namespace,
+			}, deployment)
+			Expect(err).To(BeNil())
+			Expect(deployment.Name).To(Equal(cfg.Name))
+			Expect(len(deployment.Spec.Template.Spec.Containers)).To(Equal(1))
+			Expect(deployment.Spec.Template.Spec.Containers[0].Image).To(Equal(cfg.Image))
 		})
 	})
 })
