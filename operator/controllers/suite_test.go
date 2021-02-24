8d25b7205cf292568c8ac471cda4ef19ffa27b21
Aaron Schlesinger <aaron@ecomaz.net>
2021-02-24 12:24:53 -0800

    commenting cluster connection code
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- operator/controllers/suite_test.go
+++ operator/controllers/suite_test.go
@@ -16,16 +16,14 @@
 package controllers
 
 import (
-	"path/filepath"
 	"testing"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"k8s.io/client-go/kubernetes/scheme"
-	"k8s.io/client-go/rest"
-	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/envtest"
 	"sigs.k8s.io/controller-runtime/pkg/envtest/printer"
+	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
 	httpv1alpha1 "github.com/kedacore/http-add-on/operator/api/v1alpha1"
 	// +kubebuilder:scaffold:imports
@@ -34,9 +32,9 @@ import (
 // These tests use Ginkgo (BDD-style Go testing framework). Refer to
 // http://onsi.github.io/ginkgo/ to learn more about Ginkgo.
 
-var cfg *rest.Config
-var k8sClient client.Client
-var testEnv *envtest.Environment
+// var cfg *rest.Config
+// var k8sClient client.Client
+// var testEnv *envtest.Environment
 
 func TestAPIs(t *testing.T) {
 	RegisterFailHandler(Fail)
@@ -47,34 +45,37 @@ func TestAPIs(t *testing.T) {
 }
 
 var _ = BeforeSuite(func(done Done) {
-	// logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
+	// The commented code in this function connects to a test Kubernetes cluster.
+	// We don't currently have tests that exercise functionality that needs a cluster,
+	// so we're keeping it commented.
+	logf.SetLogger(zap.LoggerTo(GinkgoWriter, true))
 
-	By("bootstrapping test environment")
-	useExistingCluster := true
-	testEnv = &envtest.Environment{
-		CRDDirectoryPaths:  []string{filepath.Join("..", "config", "crd", "bases")},
-		UseExistingCluster: &useExistingCluster,
-	}
+	// By("bootstrapping test environment")
+	// useExistingCluster := true
+	// testEnv = &envtest.Environment{
+	// 	CRDDirectoryPaths:  []string{filepath.Join("..", "config", "crd", "bases")},
+	// 	UseExistingCluster: &useExistingCluster,
+	// }
 
 	var err error
-	cfg, err = testEnv.Start()
-	Expect(err).ToNot(HaveOccurred())
-	Expect(cfg).ToNot(BeNil())
+	// cfg, err = testEnv.Start()
+	// Expect(err).ToNot(HaveOccurred())
+	// Expect(cfg).ToNot(BeNil())
 
 	err = httpv1alpha1.AddToScheme(scheme.Scheme)
 	Expect(err).NotTo(HaveOccurred())
 
 	// +kubebuilder:scaffold:scheme
 
-	k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
-	Expect(err).ToNot(HaveOccurred())
-	Expect(k8sClient).ToNot(BeNil())
+	// k8sClient, err = client.New(cfg, client.Options{Scheme: scheme.Scheme})
+	// Expect(err).ToNot(HaveOccurred())
+	// Expect(k8sClient).ToNot(BeNil())
 
 	close(done)
 }, 60)
 
 var _ = AfterSuite(func() {
 	By("tearing down the test environment")
-	err := testEnv.Stop()
-	Expect(err).ToNot(HaveOccurred())
+	// err := testEnv.Stop()
+	// Expect(err).ToNot(HaveOccurred())
 })
