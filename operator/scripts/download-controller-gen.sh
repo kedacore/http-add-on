74c8255976587b6ac9fd972cafddd45cb6556ded
Lucas Santos <lhs.santoss@gmail.com>
2021-02-25 16:46:46 -0300

    Use go install to install controller gen
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- operator/scripts/download-controller-gen.sh
+++ operator/scripts/download-controller-gen.sh
@@ -5,7 +5,7 @@ cd $CONTROLLER_GEN_TMP_DIR
 
 go mod init tmp
 echo "Downloading controller gen"
-go get sigs.k8s.io/controller-tools/cmd/controller-gen@v0.4.1
+go install sigs.k8s.io/controller-tools/cmd/controller-gen
 echo "Removing temp dir"
 rm -rf $CONTROLLER_GEN_TMP_DIR
 
