19d1348f18d5832c6c1d4fa0bb7d59d236396aa6
Aaron Schlesinger <aaron@ecomaz.net>
2021-03-23 14:07:50 -0700

    fixing hanging issue
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- pkg/net/mock_server.go
+++ pkg/net/mock_server.go
@@ -14,10 +14,14 @@ type TestHTTPHandlerWrapper struct {
 }
 
 func (t *TestHTTPHandlerWrapper) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.rwm.Lock()
-	defer t.rwm.Unlock()
-	t.incomingRequests = append(t.incomingRequests, *r)
 	t.hdl.ServeHTTP(w, r)
+	// log a request after the handler has been run
+	t.rwm.Lock()
+	t.incomingRequests = append(t.incomingRequests, *r)
+	// do not put this unlock in a defer or after the ServeHTTP line below,
+	// or you risk deadlock if the handler waits for something or otherwise
+	// hangs
+	t.rwm.Unlock()
 }
 
 // IncomingRequests returns a copy slice of all the requests that have been received before
