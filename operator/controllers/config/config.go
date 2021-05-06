2d1411768012be70b5e95800cdd3856a06109df6
Lucas Santos <lhs.santoss@gmail.com>
2021-05-06 20:26:02 -0300

    Make imagePullPolicy configurable (#155)
    * Make imagePullPolicy configurable
    
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>
    
    * Fix tests
    
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>
    
    * Pass on config from main
    
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>
    
    Co-authored-by: Aaron Schlesinger <70865+arschles@users.noreply.github.com>

--- operator/controllers/config/config.go
+++ operator/controllers/config/config.go
@@ -11,6 +11,7 @@ type Interceptor struct {
 	Image     string
 	ProxyPort int32
 	AdminPort int32
+	PullPolicy string
 }
 
 // NewInterceptorFromEnv gets interceptor configuration values from environment variables and/or
@@ -23,10 +24,12 @@ func NewInterceptorFromEnv() (*Interceptor, error) {
 	}
 	adminPort := env.GetInt32Or("KEDAHTTP_OPERATOR_INTERCEPTOR_ADMIN_PORT", 8090)
 	proxyPort := env.GetInt32Or("KEDAHTTP_OPERATOR_INTERCEPTOR_PROXY_PORT", 8091)
+	pullPolicy := env.GetOr("INTERCEPTOR_PULL_POLICY", "Always")
 	return &Interceptor{
 		Image:     image,
 		AdminPort: adminPort,
 		ProxyPort: proxyPort,
+		PullPolicy: pullPolicy,
 	}, nil
 }
 
@@ -34,6 +37,7 @@ func NewInterceptorFromEnv() (*Interceptor, error) {
 type ExternalScaler struct {
 	Image string
 	Port  int32
+	PullPolicy string
 }
 
 // NewExternalScalerFromEnv gets external scaler configuration values from environment variables and/or
@@ -42,11 +46,13 @@ type ExternalScaler struct {
 func NewExternalScalerFromEnv() (*ExternalScaler, error) {
 	image, err := env.Get("KEDAHTTP_OPERATOR_EXTERNAL_SCALER_IMAGE")
 	port := env.GetInt32Or("KEDAHTTP_OPERATOR_EXTERNAL_SCALER_PORT", 8091)
+	pullPolicy := env.GetOr("SCALER_PULL_POLICY", "Always")
 	if err != nil {
 		return nil, fmt.Errorf("Missing KEDAHTTP_EXTERNAL_SCALER_IMAGE")
 	}
 	return &ExternalScaler{
 		Image: image,
 		Port:  port,
+		PullPolicy: pullPolicy,
 	}, nil
 }
