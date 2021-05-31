9570de0f1b32a37601afe288d1de5fe0f6809ec9
Aaron Schlesinger <70865+arschles@users.noreply.github.com>
2021-05-31 05:11:14 -0700

    simplifying the magefile (#152)
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

---
+++ pkg/build/docker.go
@@ -0,0 +1,56 @@
+package build
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/kedacore/http-add-on/pkg/env"
+	"github.com/magefile/mage/sh"
+)
+
+const (
+	gitShaSuffix = "<keda-git-sha>"
+)
+
+func getGitSHA() (string, error) {
+	return sh.Output("git", "rev-parse", "--short", "HEAD")
+}
+
+// DockerBuild calls the following and returns the resulting error, if any:
+//
+//	docker build -t <image> -f <dockerfileLocation> <context>
+func DockerBuild(image, dockerfileLocation, context string) error {
+	return sh.RunV(
+		"docker",
+		"build",
+		"-t",
+		image,
+		"-f",
+		dockerfileLocation,
+		".",
+	)
+}
+
+// DockerPush calls the following and returns the resulting error, if any:
+//
+//	docker push <image>
+func DockerPush(image string) error {
+	return sh.RunV("docker", "push", image)
+}
+
+func GetImageName(envName string) (string, error) {
+	img, err := env.Get(envName)
+	if err != nil {
+		return "", err
+	}
+	if strings.HasSuffix(img, gitShaSuffix) {
+		sha, err := getGitSHA()
+		if err != nil {
+			return "", err
+		}
+		fmt.Println("using sha ", sha)
+		trimmed := strings.TrimSuffix(img, gitShaSuffix)
+		img = fmt.Sprintf("%ssha-%s", trimmed, sha)
+	}
+	return img, nil
+}
