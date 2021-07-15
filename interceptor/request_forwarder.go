f80ea9cf02caf007b867164a94489d45e4d4928e
Aaron Schlesinger <aaron@ecomaz.net>
2021-07-15 15:16:53 -0700

    adding explanation comment for slightly convoluted error generation code
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- interceptor/request_forwarder.go
+++ interceptor/request_forwarder.go
@@ -26,6 +26,9 @@ func forwardRequest(
 	}
 	proxy.ErrorHandler = func(w http.ResponseWriter, r *http.Request, err error) {
 		w.WriteHeader(502)
+		// note: we can only use the '%w' directive inside of fmt.Errorf,
+		// not Sprintf or anything similar. this means we have to create the
+		// failure string in this slightly convoluted way.
 		errMsg := fmt.Errorf("error on backend (%w)", err).Error()
 		w.Write([]byte(errMsg))
 	}
