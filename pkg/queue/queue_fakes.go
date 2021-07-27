fe3d2db2bb617bd9aa70fe945801547fb436efc1
Aaron Schlesinger <aaron@ecomaz.net>
2021-07-27 13:43:54 -0700

    adding return counts map to fake queue counter
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- pkg/queue/queue_fakes.go
+++ pkg/queue/queue_fakes.go
@@ -1,5 +1,7 @@
 package queue
 
+import "fmt"
+
 var _ Counter = &FakeCounter{}
 
 type HostAndCount struct {
@@ -7,28 +9,35 @@ type HostAndCount struct {
 	Count int
 }
 type FakeCounter struct {
+	RetMap    map[string]int
 	ResizedCh chan HostAndCount
 }
 
 func NewFakeCounter() *FakeCounter {
 	return &FakeCounter{
+		RetMap:    map[string]int{},
 		ResizedCh: make(chan HostAndCount),
 	}
 }
 
 func (f *FakeCounter) Resize(host string, i int) error {
+	f.RetMap[host] = i
 	f.ResizedCh <- HostAndCount{Host: host, Count: i}
 	return nil
 }
 
 func (f *FakeCounter) Ensure(host string) {
+	f.RetMap[host] = 0
 }
 
 func (f *FakeCounter) Current() (*Counts, error) {
 	ret := NewCounts()
-	ret.Counts = map[string]int{
-		"sample.com": 0,
+	retMap := f.RetMap
+	if len(retMap) == 0 {
+		retMap["sample.com"] = 0
 	}
+	ret.Counts = retMap
+	fmt.Println("FAKE QUEUE COUNTS RETURN", retMap)
 	return ret, nil
 }
 
