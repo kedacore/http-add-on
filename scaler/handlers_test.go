c1f52b3fc09ba7c659e3e6065499fa9e95cae418
Aaron Schlesinger <aaron@ecomaz.net>
2021-07-27 12:06:03 -0700

    starting tests for scaler handlers
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- scaler/handlers_test.go
+++ scaler/handlers_test.go
@@ -1,1 +1,56 @@
 package main
+
+import (
+	context "context"
+	"strconv"
+	"testing"
+
+	"github.com/go-logr/logr"
+	externalscaler "github.com/kedacore/http-add-on/proto"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIsActive(t *testing.T) {
+	r := require.New(t)
+	ctx := context.Background()
+	lggr := logr.Discard()
+	ticker, pinger := newFakeQueuePinger(ctx, lggr)
+	defer ticker.Stop()
+
+	hdl := newImpl(
+		lggr,
+		pinger,
+	)
+	res, err := hdl.IsActive(
+		ctx,
+		&externalscaler.ScaledObjectRef{},
+	)
+	r.NoError(err)
+	r.True(res.Result)
+}
+
+func TestGetMetricSpec(t *testing.T) {
+	const (
+		host   = "abcd"
+		target = int64(200)
+	)
+	r := require.New(t)
+	ctx := context.Background()
+	lggr := logr.Discard()
+	ticker, pinger := newFakeQueuePinger(ctx, lggr)
+	defer ticker.Stop()
+	hdl := newImpl(lggr, pinger)
+	meta := map[string]string{
+		"host":   host,
+		"target": strconv.Itoa(int(target)),
+	}
+	ref := &externalscaler.ScaledObjectRef{
+		ScalerMetadata: meta,
+	}
+	ret, err := hdl.GetMetricSpec(ctx, ref)
+	r.NoError(err)
+	r.Equal(1, len(ret.MetricSpecs))
+	spec := ret.MetricSpecs[0]
+	r.Equal(host, spec.MetricName)
+	r.Equal(target, spec.TargetSize)
+}
