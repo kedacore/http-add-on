50556b57a407ab461c8aef4d6c9a6d00b4cc05ab
Aaron Schlesinger <aaron@ecomaz.net>
2021-07-28 11:47:31 -0700

    Starting on operator config tests
    And removing dead code
    
    Signed-off-by: Aaron Schlesinger <aaron@ecomaz.net>

--- operator/controllers/config/app_info.go
+++ operator/controllers/config/app_info.go
@@ -6,12 +6,6 @@ import (
 	"github.com/kedacore/http-add-on/operator/api/v1alpha1"
 )
 
-// DeploymentName is a convenience function for
-// a.HTTPScaledObject.Spec.ScaleTargetRef.Deployment
-func DeploymentName(httpso v1alpha1.HTTPScaledObject) string {
-	return httpso.Spec.ScaleTargetRef.Deployment
-}
-
 // AppInfo contains configuration for the Interceptor and External Scaler, and holds
 // data about the name and namespace of the scale target.
 type AppInfo struct {
