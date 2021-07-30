95c45a389c44e5e4f5e26e8271b27cb72559c20b
Aaron Schlesinger <70865+arschles@users.noreply.github.com>
2021-07-29 17:20:04 -0700

    adding scaledobject check
    Signed-off-by: Aaron Schlesinger <70865+arschles@users.noreply.github.com>

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -5,9 +5,9 @@ import (
 	"fmt"
 	"testing"
 
+	"github.com/kedacore/http-add-on/pkg/k8s"
 	"github.com/kelseyhightower/envconfig"
 	"github.com/stretchr/testify/require"
-	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/util/uuid"
 )
 
@@ -33,16 +33,24 @@ func TestE2E(t *testing.T) {
 		teardown(t, ns)
 	})
 
+	cl, _ /*restCfg*/, err := getClient()
+	r.NoError(err)
+
+	// ensure that the HTTPScaledObject has a proper status,
+	// and that a ScaledObject was created for the example app
+	scaledObjectName := fmt.Sprintf("%s-app", "xkcd")
+	scaledObject, err := k8s.NewScaledObject(ns, scaledObjectName, "", "", "", 1, 2)
+	r.NoError(err)
+	r.NoError(cl.Get(ctx, objKey(ns, scaledObjectName), scaledObject))
+
 	// get the address of the interceptor proxy service and ping it.
 	// it should make a request all the way back to the example app
-	cl, restCfg, err := getClient()
-	r.NoError(err)
-	proxySvcName := "keda-add-ons-http-interceptor-proxy"
-	proxySvc := &corev1.Service{}
-	r.NoError(cl.Get(ctx, objKey(ns, proxySvcName), proxySvc))
-	pf, err := tunnelSvc(t, ctx, restCfg, proxySvc)
-	r.NoError(err)
-	defer pf.Close()
-	r.NoError(pf.ForwardPorts())
+	// proxySvcName := "keda-add-ons-http-interceptor-proxy"
+	// proxySvc := &corev1.Service{}
+	// r.NoError(cl.Get(ctx, objKey(ns, proxySvcName), proxySvc))
+	// pf, err := tunnelSvc(t, ctx, restCfg, proxySvc)
+	// r.NoError(err)
+	// defer pf.Close()
+	// r.NoError(pf.ForwardPorts())
 
 }
