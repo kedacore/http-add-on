fc9984febb245e3ac312896d1613504b07c2faad
Jorge Turrado <jorge_turrado@hotmail.es>
2024-04-30 19:58:23 +0200

    use magic string during test to prevent not detected chained changes
    Signed-off-by: Jorge Turrado <jorge_turrado@hotmail.es>

--- operator/controllers/http/suite_test.go
+++ operator/controllers/http/suite_test.go
@@ -147,7 +147,7 @@ func newCommonTestInfraWithSkipScaledObjectCreation(namespace, appName string) *
 			Namespace: namespace,
 			Name:      appName,
 			Annotations: map[string]string{
-				SkipScaledObjectCreationAnnotation: "true",
+				"httpscaledobject.keda.sh/skip-scaledobject-creation": "true",
 			},
 		},
 		Spec: httpv1alpha1.HTTPScaledObjectSpec{
