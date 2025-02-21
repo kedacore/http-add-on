7920017b24a26fe2db43d266b5139cafda29c7d1
Jan Wozniak <wozniak.jan@gmail.com>
2025-02-21 12:52:49 +0100

    make generate manifests
    Signed-off-by: Jan Wozniak <wozniak.jan@gmail.com>

--- operator/apis/http/v1alpha1/zz_generated.deepcopy.go
+++ operator/apis/http/v1alpha1/zz_generated.deepcopy.go
@@ -161,6 +161,11 @@ func (in *HTTPScaledObjectSpec) DeepCopyInto(out *HTTPScaledObjectSpec) {
 		*out = new(int32)
 		**out = **in
 	}
+	if in.InitialCooldownPeriod != nil {
+		in, out := &in.InitialCooldownPeriod, &out.InitialCooldownPeriod
+		*out = new(int32)
+		**out = **in
+	}
 	if in.ScalingMetric != nil {
 		in, out := &in.ScalingMetric, &out.ScalingMetric
 		*out = new(ScalingMetricSpec)
