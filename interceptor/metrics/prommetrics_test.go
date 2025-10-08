fea4507a4faac902006bd7114dce844880515005
Zbynek Roubalik <zroubalik@gmail.com>
2025-10-08 15:13:46 +0200

    fix: update Prometheus metrics output format for consistency
    Signed-off-by: Zbynek Roubalik <zroubalik@gmail.com>

--- interceptor/metrics/prommetrics_test.go
+++ interceptor/metrics/prommetrics_test.go
@@ -17,11 +17,8 @@ func TestPromRequestCountMetric(t *testing.T) {
 	expectedOutput := `
 	# HELP interceptor_request_count_total a counter of requests processed by the interceptor proxy
 	# TYPE interceptor_request_count_total counter
-	interceptor_request_count_total{code="500",host="test-host",method="post",otel_scope_name="keda-interceptor-proxy",otel_scope_version="",path="/test"} 1
-	interceptor_request_count_total{code="200",host="test-host",method="post",otel_scope_name="keda-interceptor-proxy",otel_scope_version="",path="/test"} 1
-	# HELP otel_scope_info Instrumentation Scope metadata
-	# TYPE otel_scope_info gauge
-	otel_scope_info{otel_scope_name="keda-interceptor-proxy",otel_scope_version=""} 1
+    interceptor_request_count_total{code="500",host="test-host",method="post",otel_scope_name="keda-interceptor-proxy",otel_scope_schema_url="",otel_scope_version="",path="/test"} 1
+    interceptor_request_count_total{code="200",host="test-host",method="post",otel_scope_name="keda-interceptor-proxy",otel_scope_schema_url="",otel_scope_version="",path="/test"} 1
 	# HELP target_info Target metadata
 	# TYPE target_info gauge
 	target_info{"service.name"="interceptor-proxy","service.version"="main"} 1
@@ -40,10 +37,7 @@ func TestPromPendingRequestCountMetric(t *testing.T) {
 	expectedOutput := `
 	# HELP interceptor_pending_request_count a count of requests pending forwarding by the interceptor proxy
 	# TYPE interceptor_pending_request_count gauge
-	interceptor_pending_request_count{host="test-host",otel_scope_name="keda-interceptor-proxy",otel_scope_version=""} 10
-	# HELP otel_scope_info Instrumentation Scope metadata
-	# TYPE otel_scope_info gauge
-	otel_scope_info{otel_scope_name="keda-interceptor-proxy",otel_scope_version=""} 1
+	interceptor_pending_request_count{host="test-host",otel_scope_name="keda-interceptor-proxy",otel_scope_schema_url="",otel_scope_version=""} 10
 	# HELP target_info Target metadata
 	# TYPE target_info gauge
 	target_info{"service.name"="interceptor-proxy","service.version"="main"} 1
